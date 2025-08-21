e2259db3de6a2514912cc49053f184860c156980
immanuelle-leonhart <ericrichardbishop@gmail.com>
2025-08-21 13:26:14 -0700

    q

--- GaianNodaTimeWrappers/GaianLocalDateTime.cs
+++ GaianNodaTimeWrappers/GaianLocalDateTime.cs
@@ -34,6 +34,8 @@ namespace Gaian
     {
         private readonly LocalDateTime _ldt;
 
+        public LocalDateTime Value => _ldt;
+
         // ===== Constructors (mirror) =====
         public GaianLocalDateTime(int year, int month, int day, int hour, int minute)
         {
