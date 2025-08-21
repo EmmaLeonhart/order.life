6bfdcba73e1c884e7e51c773dc817a028c49cba2
immanuelle-leonhart <ericrichardbishop@gmail.com>
2025-08-20 18:20:17 -0700

    Update GaianLocalDateTime.cs

--- DateTimeNodaTimeExperiments/GaianLocalDateTime.cs
+++ DateTimeNodaTimeExperiments/GaianLocalDateTime.cs
@@ -3,10 +3,233 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System;
+using System.Xml;
+using System.Xml.Schema;
+using System.Numerics;               // for generic math operator interfaces
+using NodaTime;
+using NodaTime.Calendars;
+using NodaTime.TimeZones;
+using System.Xml.Serialization;
 
 namespace Gaian
 {
-    internal class GaianLocalDateTime
+    /// <summary>
+    /// Gaian-local date and time wrapper mirroring NodaTime.LocalDateTime (3.2.x).
+    /// </summary>
+    public readonly struct GaianLocalDateTime :
+        IEquatable<GaianLocalDateTime>,
+        IComparable<GaianLocalDateTime>,
+        IComparable,
+        IFormattable,
+        IXmlSerializable,
+        IAdditionOperators<GaianLocalDateTime, Period, GaianLocalDateTime>,
+        ISubtractionOperators<GaianLocalDateTime, GaianLocalDateTime, Period>,
+        ISubtractionOperators<GaianLocalDateTime, Period, GaianLocalDateTime>,
+        IComparisonOperators<GaianLocalDateTime, GaianLocalDateTime, bool>,
+        IEqualityOperators<GaianLocalDateTime, GaianLocalDateTime, bool>
     {
+        // ===== Constructors (mirror) =====
+        public GaianLocalDateTime(int year, int month, int day, int hour, int minute)
+            => throw new NotImplementedException();
+
+        public GaianLocalDateTime(int year, int month, int day, int hour, int minute, CalendarSystem calendar)
+            => throw new NotImplementedException();
+
+        public GaianLocalDateTime(int year, int month, int day, int hour, int minute, int second)
+            => throw new NotImplementedException();
+
+        public GaianLocalDateTime(int year, int month, int day, int hour, int minute, int second, CalendarSystem calendar)
+            => throw new NotImplementedException();
+
+        public GaianLocalDateTime(int year, int month, int day, int hour, int minute, int second, int millisecond)
+            => throw new NotImplementedException();
+
+        public GaianLocalDateTime(int year, int month, int day, int hour, int minute, int second, int millisecond, CalendarSystem calendar)
+            => throw new NotImplementedException();
+
+        // ===== Properties (mirror) =====
+        public CalendarSystem Calendar => throw new NotImplementedException();
+        public int ClockHourOfHalfDay => throw new NotImplementedException();
+        public LocalDate Date => throw new NotImplementedException();
+        public int Day => throw new NotImplementedException();
+        public IsoDayOfWeek DayOfWeek => throw new NotImplementedException();
+        public int DayOfYear => throw new NotImplementedException();
+        public Era Era => throw new NotImplementedException();
+        public int Hour => throw new NotImplementedException();
+
+        // Max/Min values (ISO)
+        public static GaianLocalDateTime MaxIsoValue => throw new NotImplementedException();
+        public static GaianLocalDateTime MinIsoValue => throw new NotImplementedException();
+
+        public int Millisecond => throw new NotImplementedException();
+        public int Minute => throw new NotImplementedException();
+        public int Month => throw new NotImplementedException();
+        public long NanosecondOfDay => throw new NotImplementedException();
+        public int NanosecondOfSecond => throw new NotImplementedException();
+        public int Second => throw new NotImplementedException();
+        public long TickOfDay => throw new NotImplementedException();
+        public int TickOfSecond => throw new NotImplementedException();
+        public LocalTime TimeOfDay => throw new NotImplementedException();
+        public int Year => throw new NotImplementedException();
+        public int YearOfEra => throw new NotImplementedException();
+
+        // ===== Static methods (mirror) =====
+        public static GaianLocalDateTime Add(GaianLocalDateTime localDateTime, Period period)
+            => throw new NotImplementedException();
+
+        public static XmlQualifiedName AddSchema(XmlSchemaSet xmlSchemaSet)
+            => throw new NotImplementedException();
+
+        public static GaianLocalDateTime FromDateTime(DateTime dateTime)
+            => throw new NotImplementedException();
+
+        public static GaianLocalDateTime FromDateTime(DateTime dateTime, CalendarSystem calendar)
+            => throw new NotImplementedException();
+
+        public static GaianLocalDateTime Max(GaianLocalDateTime x, GaianLocalDateTime y)
+            => throw new NotImplementedException();
+
+        public static GaianLocalDateTime Min(GaianLocalDateTime x, GaianLocalDateTime y)
+            => throw new NotImplementedException();
+
+        public static Period Subtract(GaianLocalDateTime lhs, GaianLocalDateTime rhs)
+            => throw new NotImplementedException();
+
+        public static GaianLocalDateTime Subtract(GaianLocalDateTime localDateTime, Period period)
+            => throw new NotImplementedException();
+
+        // ===== Instance methods (mirror) =====
+        public bool Equals(GaianLocalDateTime other)
+            => throw new NotImplementedException();
+
+        public override bool Equals(object? obj)
+            => throw new NotImplementedException();
+
+        public override int GetHashCode()
+            => throw new NotImplementedException();
+
+        public ZonedDateTime InUtc()
+            => throw new NotImplementedException();
+
+        public ZonedDateTime InZone(DateTimeZone zone, ZoneLocalMappingResolver resolver)
+            => throw new NotImplementedException();
+
+        public ZonedDateTime InZoneLeniently(DateTimeZone zone)
+            => throw new NotImplementedException();
+
+        public ZonedDateTime InZoneStrictly(DateTimeZone zone)
+            => throw new NotImplementedException();
+
+        public GaianLocalDateTime Minus(Period period)
+            => throw new NotImplementedException();
+
+        public Period Minus(GaianLocalDateTime localDateTime)
+            => throw new NotImplementedException();
+
+        public GaianLocalDateTime Next(IsoDayOfWeek targetDayOfWeek)
+            => throw new NotImplementedException();
+
+        public GaianLocalDateTime Previous(IsoDayOfWeek targetDayOfWeek)
+            => throw new NotImplementedException();
+
+        public GaianLocalDateTime Plus(Period period)
+            => throw new NotImplementedException();
+
+        public GaianLocalDateTime PlusDays(int days)
+            => throw new NotImplementedException();
+
+        public GaianLocalDateTime PlusHours(long hours)
+            => throw new NotImplementedException();
+
+        public GaianLocalDateTime PlusMilliseconds(long milliseconds)
+            => throw new NotImplementedException();
+
+        public GaianLocalDateTime PlusMinutes(long minutes)
+            => throw new NotImplementedException();
+
+        public GaianLocalDateTime PlusMonths(int months)
+            => throw new NotImplementedException();
+
+        public GaianLocalDateTime PlusNanoseconds(long nanoseconds)
+            => throw new NotImplementedException();
+
+        public GaianLocalDateTime PlusSeconds(long seconds)
+            => throw new NotImplementedException();
+
+        public GaianLocalDateTime PlusTicks(long ticks)
+            => throw new NotImplementedException();
+
+        public GaianLocalDateTime PlusWeeks(int weeks)
+            => throw new NotImplementedException();
+
+        public GaianLocalDateTime PlusYears(int years)
+            => throw new NotImplementedException();
+
+        public DateTime ToDateTimeUnspecified()
+            => throw new NotImplementedException();
+
+        public override string ToString()
+            => throw new NotImplementedException();
+
+        public string ToString(string? patternText, IFormatProvider? formatProvider)
+            => throw new NotImplementedException();
+
+        public GaianLocalDateTime With(Func<LocalDate, LocalDate> adjuster)
+            => throw new NotImplementedException();
+
+        public GaianLocalDateTime With(Func<LocalTime, LocalTime> adjuster)
+            => throw new NotImplementedException();
+
+        public GaianLocalDateTime WithCalendar(CalendarSystem calendar)
+            => throw new NotImplementedException();
+
+        public OffsetDateTime WithOffset(Offset offset)
+            => throw new NotImplementedException();
+
+        // ===== Comparison (mirror) =====
+        public int CompareTo(GaianLocalDateTime other)
+            => throw new NotImplementedException();
+
+        int IComparable.CompareTo(object? obj)
+            => throw new NotImplementedException();
+
+        // ===== Operators (mirror) =====
+        public static GaianLocalDateTime operator +(GaianLocalDateTime localDateTime, Period period)
+            => throw new NotImplementedException();
+
+        public static bool operator ==(GaianLocalDateTime left, GaianLocalDateTime right)
+            => throw new NotImplementedException();
+
+        public static bool operator !=(GaianLocalDateTime left, GaianLocalDateTime right)
+            => throw new NotImplementedException();
+
+        public static bool operator >(GaianLocalDateTime lhs, GaianLocalDateTime rhs)
+            => throw new NotImplementedException();
+
+        public static bool operator >=(GaianLocalDateTime lhs, GaianLocalDateTime rhs)
+            => throw new NotImplementedException();
+
+        public static bool operator <(GaianLocalDateTime lhs, GaianLocalDateTime rhs)
+            => throw new NotImplementedException();
+
+        public static bool operator <=(GaianLocalDateTime lhs, GaianLocalDateTime rhs)
+            => throw new NotImplementedException();
+
+        public static Period operator -(GaianLocalDateTime lhs, GaianLocalDateTime rhs)
+            => throw new NotImplementedException();
+
+        public static GaianLocalDateTime operator -(GaianLocalDateTime localDateTime, Period period)
+            => throw new NotImplementedException();
+
+        // ===== XML serialization (explicit) =====
+        XmlSchema? IXmlSerializable.GetSchema()
+            => throw new NotImplementedException();
+
+        void IXmlSerializable.ReadXml(XmlReader reader)
+            => throw new NotImplementedException();
+
+        void IXmlSerializable.WriteXml(XmlWriter writer)
+            => throw new NotImplementedException();
     }
 }
